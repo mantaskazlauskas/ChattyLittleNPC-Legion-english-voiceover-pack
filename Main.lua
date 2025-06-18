e630d4424be07efba4fc87c0a00cd5fe17783ff3
Tuubby <mmantas.kazlauskas@gmail.com>
2025-06-18 11:34:14 +0300

    Init commit

---
+++ Main.lua
@@ -0,0 +1,9 @@
+---@class ChattyLittleNpc_Legion_voiceovers
+local ChattyLittleNpc_Legion_voiceovers = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_Legion_voiceovers")
+
+ChattyLittleNpc_Legion_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = false,
+    ContainsGameObjectVoiceovers = false,
+    ContainsItemVoiceovers = false
+}
